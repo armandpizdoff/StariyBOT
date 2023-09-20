2c960ff7f15860a416618be41a3c06a21c6823d4
armandpizdoff <daveton@yandex.ru>
2023-09-20 23:28:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -563,6 +563,8 @@ def get_text_messages(message):
         textcommand.get_text_messages(message)
     elif message.text == 'скрытое отхлищивание...':
         textcommand.get_text_messages(message)
+    elif message.text == 'ты лох':
+        textcommand.get_text_messages(message)
 
 
 bot.polling(none_stop=True, interval=0)
